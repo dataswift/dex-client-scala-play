95c52fd4c21bc7be2b92867dcde924579d3bdad4
andrius <andrius.aucinas@gmail.com>
2017-09-21 15:29:02 +0100

    refactors V2 offers data models

---
+++ src/main/scala/org/hatdex/dex/apiV2/json/DexJsonFormats.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2016 HAT Data Exchange Ltd - All Rights Reserved
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * Written by Andrius Aucinas <andrius.aucinas@hatdex.org>, 2 / 2017
+ *
+ */
+
+package org.hatdex.dex.apiV2.json
+
+import org.hatdex.hat.api.json.{ DataDebitFormats, HatJsonFormats }
+import org.hatdex.dex.apiV2.models._
+import org.hatdex.hat.api.models.RichDataJsonFormats
+import org.joda.time.Duration
+import play.api.data.validation.ValidationError
+import play.api.libs.json._
+
+trait DexJsonFormats extends DataDebitFormats with RichDataJsonFormats {
+
+  /*
+  * Duration Json formats
+   */
+  implicit val durationWrites: Writes[Duration] = new Writes[Duration] {
+    def writes(o: Duration): JsValue = JsNumber(o.getMillis)
+  }
+  implicit val durationReads: Reads[Duration] = new Reads[Duration] {
+    def reads(json: JsValue): JsResult[Duration] = json match {
+      case JsNumber(value) => JsSuccess(new Duration(value.toLong))
+      case _               => JsError(Seq(JsPath() -> Seq(ValidationError("validate.error.expected.period"))))
+    }
+  }
+
+  implicit val offervHatFormat = Json.format[OfferHat]
+  implicit val offervClaimFormat = Json.format[OfferClaimSummary]
+  implicit val offervHatCredentialsFormat = Json.format[OfferHatCredentialsSummary]
+  implicit val offervClaimsInfoFormat = Json.format[OfferClaimsInfo]
+
+  implicit val offerRegistrationFormat: Format[OfferRegistration] = Json.format[OfferRegistration]
+  implicit val offerFormat: Format[Offer] = Json.format[Offer]
+}
+
+object DexJsonFormats extends DexJsonFormats
---
+++ src/main/scala/org/hatdex/dex/apiV2/services/DexOffers.scala
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2016 HAT Data Exchange Ltd - All Rights Reserved
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * Written by Andrius Aucinas <andrius.aucinas@hatdex.org>, 2 / 2017
+ *
+ */
+
+package org.hatdex.dex.apiV2.services
+
+import java.util.UUID
+
+import org.hatdex.dex.apiV2.json.DexJsonFormats
+import org.hatdex.dex.apiV2.models.OfferClaimsInfo
+import play.api.Logger
+import play.api.http.Status._
+import play.api.libs.ws._
+
+import scala.concurrent.{ ExecutionContext, Future }
+
+trait DexOffers {
+  val logger: Logger
+  val ws: WSClient
+  val schema: String
+  val dexAddress: String
+
+  import DexJsonFormats.offervClaimsInfoFormat
+
+  def offerClaims(access_token: String, offerId: UUID)(implicit ec: ExecutionContext): Future[OfferClaimsInfo] = {
+    logger.debug(s"Get Data Debit $offerId values from $dexAddress")
+
+    val request: WSRequest = ws.url(s"$schema$dexAddress/api/offer/$offerId/claims")
+      .withVirtualHost(dexAddress)
+      .withHeaders("Accept" -> "application/json", "X-Auth-Token" -> access_token)
+
+    val futureResponse: Future[WSResponse] = request.get()
+    futureResponse.map { response =>
+      response.status match {
+        case OK =>
+          val jsResponse = response.json.validate[OfferClaimsInfo] recover {
+            case e =>
+              logger.error(s"Error parsing successful offer claims info response: ${e}")
+              throw new RuntimeException(s"Error parsing successful offer claims info response: ${e}")
+          }
+          // Convert to OfferClaimsInfo - if validation has failed, it will have thrown an error already
+          jsResponse.get
+        case _ =>
+          logger.error(s"Fetching Offer $offerId claims from $dexAddress failed, $response, ${response.body}")
+          throw new RuntimeException(s"Fetching Offer $offerId claims from $dexAddress failed, $response, ${response.body}")
+      }
+    }
+  }
+
+}
