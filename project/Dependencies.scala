1a86c6973e05aeac62bbcda726e800e37fa05db2
andrius <andrius.aucinas@gmail.com>
2017-09-25 18:27:34 +0100

    bumps up HAT API client version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -41,7 +41,7 @@ object Dependencies {
     }
 
     object HATDeX {
-      private val version = "2.4.0-SNAPSHOT"
+      private val version = "2.4.1-SNAPSHOT"
       val hatClient = "org.hatdex" %% "hat-client-scala-play" % version
     }
   }
