910d85455205db81cf2caa86356d23212153eb0a
Terry Lee <terry.lee@hatdex.org>
2018-08-17 11:21:55 +0100

    Update dependency to hat-client-scala-play:2.6.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -43,7 +43,7 @@ object Dependencies {
     }
 
     object HATDeX {
-      private val version = "2.6.2-SNAPSHOT"
+      private val version = "2.6.3-SNAPSHOT"
       val hatClient = "org.hatdex" %% "hat-client-scala-play" % version
     }
   }
