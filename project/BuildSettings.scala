53c398e8a84d5c54d4a1996f60a76937b7100f86
Marios Tsekis <marios4th@yahoo.gr>
2019-11-12 12:38:37 +0000

    changed hat client library version and project version

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -18,7 +18,7 @@ object BasicSettings extends AutoPlugin {
 
   override def projectSettings = Seq(
     organization := "org.hatdex",
-    version := "2.6.6-SNAPSHOT",
+    version := "2.6.7-SNAPSHOT",
     resolvers ++= Dependencies.resolvers,
     scalaVersion := Dependencies.Versions.scalaVersion,
     crossScalaVersions := Dependencies.Versions.crossScala,
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -43,7 +43,7 @@ object Dependencies {
     }
 
     object HATDeX {
-      private val version = "2.6.3-SNAPSHOT"
+      private val version = "2.6.8-SNAPSHOT"
       val hatClient = "org.hatdex" %% "hat-client-scala-play" % version
     }
   }
