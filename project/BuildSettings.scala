224b8b03cf65b042722afea56c888dae3698cb05
Ty <tyler.weir@dataswift.io>
2020-10-22 15:01:19 -0400

    DSE-608 #comment update the version of the library.

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -19,48 +19,44 @@ object BasicSettings extends AutoPlugin {
   override def projectSettings: Seq[Def.Setting[_]] =
     Seq(
       organization := "org.hatdex",
-      version := "2.6.11",
+      version := "2.6.12",
       resolvers ++= Dependencies.resolvers,
       scalaVersion := Dependencies.Versions.scalaVersion,
       crossScalaVersions := Dependencies.Versions.crossScala,
       name := "DEX Client Scala",
       description := "Dataswift Exchange HTTP API wrapper for Scala",
-      licenses += ("Mozilla Public License 2.0", url(
-        "https://www.mozilla.org/en-US/MPL/2.0")),
+      licenses += ("Mozilla Public License 2.0", url("https://www.mozilla.org/en-US/MPL/2.0")),
       scmInfo := Some(
-        ScmInfo(
-          url("https://github.com/Hub-of-all-Things/dex-client-scala-play"),
-          "scm:git@github.com:Hub-of-all-Things/dex-client-scala-play.git"
-        )
-      ),
+            ScmInfo(
+              url("https://github.com/Hub-of-all-Things/dex-client-scala-play"),
+              "scm:git@github.com:Hub-of-all-Things/dex-client-scala-play.git"
+            )
+          ),
       homepage := Some(url("https://dataswift.io")),
       developers := List(
-        Developer(
-          id = "AndriusA",
-          name = "Andrius Aucinas",
-          email = "andrius@smart-e.org",
-          url = url("http://smart-e.org")
-        ),
-        Developer(
-          id = "AugustinasM",
-          name = "Augustinas Markevicius",
-          email = "gus@codeandpicture.com",
-          url = url("http://codeandpicture.com")
-        )
-      ),
+            Developer(
+              id = "AndriusA",
+              name = "Andrius Aucinas",
+              email = "andrius@smart-e.org",
+              url = url("http://smart-e.org")
+            ),
+            Developer(
+              id = "AugustinasM",
+              name = "Augustinas Markevicius",
+              email = "gus@codeandpicture.com",
+              url = url("http://codeandpicture.com")
+            )
+          ),
       scalacOptions ++= Seq(
-        "-deprecation", // Emit warning and location for usages of deprecated APIs.
-        "-feature", // Emit warning and location for usages of features that should be imported explicitly.
-        "-unchecked", // Enable additional warnings where generated code depends on assumptions.
-        "-Xfatal-warnings", // Fail the compilation if there are any warnings.
-        "-Xlint", // Enable recommended additional warnings.
-        //"-Ywarn-adapted-args", // Warn if an argument list is modified to match the receiver.
-        "-Ywarn-dead-code", // Warn when dead code is identified.
-        //"-Ywarn-inaccessible", // Warn about inaccessible types in method signatures.
-        //"-Ywarn-nullary-override", // Warn when non-nullary overrides nullary, e.g. def foo() over def foo.
-        "-language:postfixOps", // Allow postfix operators
-        "-Ywarn-numeric-widen" // Warn when numerics are widened.
-      ),
+            "-deprecation", // Emit warning and location for usages of deprecated APIs.
+            "-feature", // Emit warning and location for usages of features that should be imported explicitly.
+            "-unchecked", // Enable additional warnings where generated code depends on assumptions.
+            "-Xfatal-warnings", // Fail the compilation if there are any warnings.
+            "-Xlint", // Enable recommended additional warnings.
+            "-Ywarn-dead-code", // Warn when dead code is identified.
+            "-language:postfixOps", // Allow postfix operators
+            "-Ywarn-numeric-widen" // Warn when numerics are widened.
+          ),
       scalacOptions in Test ~= { (options: Seq[String]) =>
         options filterNot (_ == "-Ywarn-dead-code") // Allow dead code in tests (to support using mockito).
       },
