4a92388ee0c747ed53e63831181959a15ce552af
Piotr Perzyna <piotr.perzyna@dataswift.io>
2021-01-13 14:54:57 +0100

    removed constant version

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -19,7 +19,6 @@ object BasicSettings extends AutoPlugin {
   override def projectSettings: Seq[Def.Setting[_]] =
     Seq(
       organization := "org.hatdex",
-      version := "3.0.0",
       resolvers ++= Dependencies.resolvers,
       scalaVersion := Dependencies.Versions.scalaVersion,
       crossScalaVersions := Dependencies.Versions.crossScala,
